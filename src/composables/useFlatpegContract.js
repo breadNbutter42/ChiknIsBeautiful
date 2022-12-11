dc61e861ce6911c537c20de9bdcb854a7e77cce5
kitfuntastik <39065357+kitfuntastik@users.noreply.github.com>
2022-12-11 03:44:42 -0500

    git commits

--- src/composables/useFlatpegContract.js
+++ src/composables/useFlatpegContract.js
@@ -14,13 +14,51 @@ export default (address) => {
   /*
   const symbol = async () => await contract.symbol()
   //0 inputs, read
-  const approve = async (_number) => await contract.approve(VITE_CONTRACT_FLATPEG, ethers.utils.parseEther(String(_number)))
+  const approve = async (_number) => await contract.approve(VITE_CONTRACT_THIRD, ethers.utils.parseEther(String(_number)))
   //two input spender / amount; Write
-  const allowance = async (payload) => await contract.allowance(payload ?? address.value, VITE_CONTRACT_FLATPEG).then(response => Number(ethers.utils.formatEther(response)))
-//two var input : owner / spender; read
+  const allowance = async (payload) => await contract.allowance(payload ?? address.value, VITE_CONTRACT_THIRD).then(response => Number(ethers.utils.formatEther(response)))
+  //two var input : owner / spender; read
   const balanceOf = async (payload) => await contract.balanceOf(payload ?? address.value).then(response => Number(ethers.utils.formatEther(response)).toFixed(2))
-//one var user address input; read
+  //one var user address input; read
   */
+
+  const claimPremint = async () => await contract.claimPremint()
+  //0 vars; Write
+
+  const setApprovalForAllALLOW = async () => await contract.setApprovalForAll(VITE_CONTRACT_THIRD, true)
+  const setApprovalForAllREVOKE = async () => await contract.setApprovalForAll(VITE_CONTRACT_THIRD, false)
+
+  //2 vars input: operator, bool; Write
+
+
+  const amountMintedDuringPremint = async () => await contract.amountMintedDuringPremint().then(response => Number(response))
+  const amountMintedByDevs = async () => await contract.amountMintedByDevs().then(response => Number(response))
+  const amountMintedDuringPublicSale = async () => await contract.amountMintedDuringPublicSale().then(response => Number(response))
+  const collectionSize = async () => await contract.collectionsSize().then(response => Number(response))
+  const name = async () => await contract.name().then(response => String(response))
+  const symbol = async () => await contract.symbol().then(response => String(response))
+  const tokenURI = async () => await contract.tokenURI().then(response => String(response))
+  const unrevealedURI = async () => await contract.unrevealedURI().then(response => String(response))
+  const totalSupply = async () => await contract.totalSupply().then(response => Number(response))
+
+
+  //0 vars input; Read
+
+    
+  const balanceOf = async (payload) => await contract.balanceOf(payload ?? address.value).then(response => Number(response))
+  //owner 
+
+  const getOwnershipDataSUPER = async (superID) => await contract.getOwnershipData(superID).then(response => String(response))
+  const getOwnershipDataCHAD = async (chadID) => await contract.getOwnershipData(chadID).then(response => String(response))
+  const getOwnershipDataVIAL = async (vialID) => await contract.getOwnershipData(vialID).then(response => String(response))
+  const getOwnershipDataUPGRADE = async (upgradeID) => await contract.getOwnershipData(upgradeID).then(response => String(response))
+
+  //tuple response for getOwnershipData
+  //https://gist.github.com/fnky/0a6cd5f39a7ad0ace79a7a4f5c999691
+
+  const isApprovedForAll = async (payload) => await contract.isApprovedForAll(payload ?? address.value, VITE_CONTRACT_THIRD).then(response => Number(response))
+  //owner / operator : bool ; Read
+
 
 
 
@@ -39,9 +77,23 @@ export default (address) => {
 
   return {//repeat the function names here again
     contract,
+    claimPremint,
+    setApprovalForAllALLOW,
+    setApprovalForAllREVOKE,
+    amountMintedDuringPremint,
+    amountMintedByDevs,
+    amountMintedDuringPublicSale,
+    collectionSize,
+    name,
     symbol,
-    approve,
-    allowance,
-    balanceOf
+    tokenURI,
+    unrevealedURI,
+    totalSupply,
+    balanceOf,
+    getOwnershipDataSUPER,
+    getOwnershipDataCHAD,
+    getOwnershipDataVIAL,
+    getOwnershipDataUPGRADE,
+    isApprovedForAll
   }
 }
--- src/composables/useThirdContract.js
+++ src/composables/useThirdContract.js
@@ -21,35 +21,40 @@ export default (address) => {
   const vialContract = async () => await contract.vialContract().then(response => String(response))
   const upgradeContract = async () => await contract.upgradeContract().then(response => String(response))
   const superContract = async () => await contract.superContract().then(response => String(response))
-  const chadContract = async () => await contract.chadContract().then(response => String(response))
-  const fVialsBurned = async () => await contract.fVialsBurned().then(response => String(response))
-  const nVialsBurned = async () => await contract.nVialsBurned().then(response => String(response))
-  const vialsBurned = async () => await contract.vialsBurned().then(response => String(response))
-  const fVialsSet = async () => await contract.fVialsSet().then(response => String(response))
-  const preminted = async () => await contract.preminted().then(response => String(response))
+  const chadContract = async () => await contract.chadContract().then(response => Number(response))
+  const fVialsBurned = async () => await contract.fVialsBurned().then(response => Number(response))
+  const nVialsBurned = async () => await contract.nVialsBurned().then(response => Number(response))
+  const vialsBurned = async () => await contract.vialsBurned().then(response => Number(response))
+  const fVialsSet = async () => await contract.fVialsSet().then(response => Boolean(response))
+  const preminted = async () => await contract.preminted().then(response => Number(response))
 
   //0 inputs, read
 
 
-  const vialToF = async (vialID) => await contract.vialToF(vialID).then(response => String(response))
-  const supersToChad = async (superID) => await contract.supersToChad(superID).then(response => String(response))
-  const superTokenURI = async (superID) => await contract.superTokenURI(superID).then(response => String(response))
-  const superToMinted = async (superID) => await contract.superToMinted(superID).then(response => String(response))
-  const chadToMinted = async (chadID) => await contract.chadToMinted(chadID).then(response => String(response))
-  const chadToSupers = async (chadID) => await contract.chadToSupers(chadID).then(response => String(response))
-  const getShuffledTokenIDSUPERS = async (superID) => await contract.getShuffledTokenID(superID).then(response => String(response))
-  const getShuffledTokenIDCHAD = async (chadID) => await contract.getShuffledTokenID(chadID).then(response => String(response))
 
-  const id2UpgradeArray = async (superID) => await contract.id2UpgradeArray(superID).then(response => String(response))
+
+
+
+  const vialToF = async (vialID) => await contract.vialToF(vialID).then(response => Number(response))
+  const supersToChad = async (superID) => await contract.supersToChad(superID).then(response => Number(response))
+  const superTokenURI = async (superID) => await contract.superTokenURI(superID).then(response => String(response))
+  const superToMinted = async (superID) => await contract.superToMinted(superID).then(response => Boolean(response))
+  const chadToMinted = async (chadID) => await contract.chadToMinted(chadID).then(response => Boolean(response))
+  const chadToSupers = async (chadID) => await contract.chadToSupers(chadID).then(response => Number(response))
+  const getShuffledTokenIDSUPERS = async (superID) => await contract.getShuffledTokenID(superID).then(response => Number(response))
+  const getShuffledTokenIDCHAD = async (chadID) => await contract.getShuffledTokenID(chadID).then(response => Number(response))
+
+  const id2UpgradeArray = async (superID) => await contract.id2UpgradeArray(superID).then(response => Array(response))
 
   //one input, read                                                    
 
-  const superToUpgradeArray = async (superID, arraySlotX) => await contract.superToUpgradeArray(superID, arraySlotX).then(response => String(response))
+  const superToUpgradeArray = async (superID, arraySlotX) => await contract.superToUpgradeArray(superID, arraySlotX).then(response => Number(response))
 
   //two input to read id and then position in array
   //read
 
 
+  
 
   userStore.$onAction(({ name, after }) => {
     after(() => {
@@ -66,15 +71,27 @@ export default (address) => {
 
   return {
     contract,
-    returnTotalVotesForCandidateIDNumber,
-    voteWithEggByCandidateNumber,
-    voteOneEggForEachCandidate,
-    prizeMoneyTotalWei,
-    eggBurntTotalWei,
-    allVotesTotalBase,
-    totalVotesFromVoterAddress,
-    votingTimeLeftBlockTimestampHours,
-    addressTotalVotesForIDNumber
-    
+    upgradeChad,
+    upgradeSuper,
+    zeroAddress,
+    vialContract,
+    upgradeContract,
+    superContract,
+    chadContract,
+    fVialsBurned,
+    nVialsBurned,
+    vialsBurned,
+    fVialsSet,
+    preminted,
+    vialToF,
+    supersToChad,
+    superTokenURI,
+    superToMinted,
+    chadToMinted,
+    chadToSupers,
+    getShuffledTokenIDSUPERS,
+    getShuffledTokenIDCHAD,
+    id2UpgradeArray,
+    superToUpgradeArray
   }
 }
