19ee22265d50706688eaba7eff60dc2469dc5991
kitfuntastik <39065357+kitfuntastik@users.noreply.github.com>
2022-12-11 01:31:30 -0500

    git commit

--- src/composables/useFlatpegContract.js
+++ src/composables/useFlatpegContract.js
@@ -11,12 +11,19 @@ export default (address) => {
   let contract
   const setContract = (payload) => contract = new ethers.Contract(VITE_CONTRACT_FLATPEG, abi, payload)
 
+  /*
   const symbol = async () => await contract.symbol()
-  const approve = async (_number) => await contract.approve(VITE_CONTRACT_VOTING, ethers.utils.parseEther(String(_number)))
-  const allowance = async (payload) => await contract.allowance(payload ?? address.value, VITE_CONTRACT_VOTING).then(response => Number(ethers.utils.formatEther(response)))
+  //0 inputs, read
+  const approve = async (_number) => await contract.approve(VITE_CONTRACT_FLATPEG, ethers.utils.parseEther(String(_number)))
+  //two input spender / amount; Write
+  const allowance = async (payload) => await contract.allowance(payload ?? address.value, VITE_CONTRACT_FLATPEG).then(response => Number(ethers.utils.formatEther(response)))
+//two var input : owner / spender; read
   const balanceOf = async (payload) => await contract.balanceOf(payload ?? address.value).then(response => Number(ethers.utils.formatEther(response)).toFixed(2))
+//one var user address input; read
+  */
 
-  
+
+
   userStore.$onAction(({ name, after }) => {
     after(() => {
       const actions = {
@@ -30,7 +37,7 @@ export default (address) => {
 
   isAuthenticated.value ? setContract(wallet.getSigner()) : setContract(wallet)
 
-  return {
+  return {//repeat the function names here again
     contract,
     symbol,
     approve,
--- src/composables/useThirdContract.js
+++ src/composables/useThirdContract.js
@@ -8,10 +8,12 @@ export default (address) => {
   const userStore = useUserStore()
 
   let contract
-  const setContract = (payload) => contract = new ethers.Contract(import.meta.env.VITE_CONTRACT_VOTING, abi, payload)
+  const setContract = (payload) => contract = new ethers.Contract(import.meta.env.VITE_CONTRACT_THIRD, abi, payload)
 
+  
   const voteWithEggByCandidateNumber = async (id, amt) => await contract.voteWithEggByCandidateNumber(id, amt)
   const voteOneEggForEachCandidate = async () => await contract.voteOneEggForEachCandidate()
+
   const returnTotalVotesForCandidateIDNumber = async (id) => await contract.returnTotalVotesForCandidateIDNumber(id).then(response => Number(response))
   
   const prizeMoneyTotalWei = async () => await contract.prizeMoneyTotalWei().then(response => String(ethers.utils.formatEther(response)))
@@ -20,8 +22,35 @@ export default (address) => {
   const votingTimeLeftBlockTimestampHours = async () => await contract.votingTimeLeftBlockTimestamp().then(response => String(Math.floor(Number(response) / 3600))).catch(() => 0)
   const totalVotesFromVoterAddress = async (payload) => await contract.totalVotesFromVoterAddress(payload ?? address.value).then(response => String(response))
   const addressTotalVotesForIDNumber = async (id, payload) => await contract.addressTotalVotesForIdNumber(payload ?? address.value, id).then(response => String(response))
-
+//this payload one is interesting
+//original VOTR contract https://snowtrace.io/address/0x33b0db1369f81c99908affc6a2fbba8f1bc99096#readContract
   
+  
+  const upgradeChad = async (vialID, chadID) => await contract.upgradeChad(vialID, chadID)
+  const upgradeSuper = async (vialID, superID) => await contract.upgradeSuper(vialID, superID)
+  
+//two inputs, write
+
+
+const chadContract = async () => await contract.chadContract().then(response => String(response))
+const fVialsBurned = async () => await contract.fVialsBurned().then(response => String(response))
+const nVialsBurned = async () => await contract.nVialsBurned().then(response => String(response))
+const fVialsSet = async () => await contract.fVialsSet().then(response => String(response))
+const preminted = async () => await contract.preminted().then(response => String(response))
+
+//0 inputs, read
+
+const chadToMinted = async (chadID) => await contract.chadToMinted(chadID).then(response => String(response))
+const chadToSupers = async (chadID) => await contract.chadToSupers(chadID).then(response => String(response))
+const getShuffledTokenID = async (superID) => await contract.getShuffledTokenID(superID).then(response => String(response))
+const id2UpgradeArray = async (superID) => await contract.id2UpgradeArray(superID).then(response => String(response))
+
+//one input, read                                                    
+
+//read
+
+
+
   userStore.$onAction(({ name, after }) => {
     after(() => {
       const actions = {
