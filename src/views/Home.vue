baa71382c4aa01da2e700ab01f3240774130c4ad
kitfuntastik <39065357+kitfuntastik@users.noreply.github.com>
2022-12-13 02:27:26 -0500

    Vsymbol test home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -193,7 +193,7 @@ onAppEvent(({ type }) => {
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 mt-4">
       <div class="px-6 py-4 shadow-sm bg-gradient-to-tr from-red-200/10 rounded-2xl flex justify-between items-center">
         <div class="text-xs font-celaraz">All Vials Burned</div>
-        <div class="font-bold"> {{ allowanceState.value.Vsymbol }} / 2185 Burned</div>
+        <div class="font-bold"> {{ state.Vsymbol }} / 2185 Burned</div>
       </div>
       <div class="px-6 py-4 shadow-sm bg-gradient-to-tr from-red-200/10 rounded-2xl flex justify-between items-center">
         <div class="text-xs font-celaraz">N Vials Burned</div>
