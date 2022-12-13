dba093dcfb29404ae6b2cbd7d0a6c3436bb9cecd
kitfuntastik <39065357+kitfuntastik@users.noreply.github.com>
2022-12-12 23:49:25 -0500

    test _Vsymbol

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -189,7 +189,7 @@ onAppEvent(({ type }) => {
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 mt-4">
       <div class="px-6 py-4 shadow-sm bg-gradient-to-tr from-red-200/10 rounded-2xl flex justify-between items-center">
         <div class="text-xs font-celaraz">All Vials Burned</div>
-        <div class="font-bold">  / 2185 Burned</div>
+        <div class="font-bold"> {{ _Vsymbol }} / 2185 Burned</div>
       </div>
       <div class="px-6 py-4 shadow-sm bg-gradient-to-tr from-red-200/10 rounded-2xl flex justify-between items-center">
         <div class="text-xs font-celaraz">N Vials Burned</div>
